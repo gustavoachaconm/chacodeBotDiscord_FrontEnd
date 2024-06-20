a86d55f05a215b2dba5f59f88c1de71c4fb90f07
Juan <juanmisiac@gmail.com>
2024-06-19 21:42:47 -0300

    cambios minimos

---
+++ src/components/SelectServer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SelectServer = () => {
+  return (
+    <div className='flex-col justify-center items-center m-3'>
+      <h3 className='font-bold text-4xl'>Selecciona un servidor</h3>
+      <div className='flex-row'>
+
+      </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { SelectServer } from './components/SelectServer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='flex-col justify-center items-center'>
+      <SelectServer />
+      <App />
+    </div>
   </React.StrictMode>,
-)
+);
