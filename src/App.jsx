c8f7962968a0f9d65b10a07ea42ddf8c14482438
Yoan <unlock.eagle@gmail.com>
2024-06-20 02:10:36 +0200

    navbar-yoan

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import NavBar from "./components/NavBar"
+
 function App() {
 
 
   return (
     <>
+    <NavBar/>
       <div className=' container h-screen content-center'>
         <div className='flex justify-center'>
           <a className='border-2 border-black rounded-lg p-3 ' href="https://discord.com/oauth2/authorize?client_id=1250839124466991136&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fprueba&scope=identify+email+guilds+applications.commands.permissions.update">Inicia sesión con Discord</a>
