bb549bda0d7f72cb011ff303bb57fcc0066c3edb
Martin Santiago Aguirre <martinavendano095@gmail.com>
2024-06-21 14:12:33 -0500

    feat:responsive hero

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,7 +5,7 @@ import example4 from "../assets/example-4.png"
 const Description = () => {
   return (
     <div className="bg-[#052E0B] grid gap-44">
-        <section className="grid grid-cols-2  ">
+        <section className="grid grid-cols-1 md:grid-cols-2  ">
             <div className=" grid content-center justify-items-center bg-[#DCFCDF]  rounded-r-[8rem] ">
                 <div className="w-[70%] m-10 grid gap-8">
                     <h2 className="font-black text-[32px] sm:text-[58px] leading-none text-[#22BC33] ">Intuitivo, Rápido y útil</h2>
@@ -14,19 +14,19 @@ const Description = () => {
                     </p>
                 </div>      
             </div>
-            <div className="grid justify-center items-center content-center">
-                <img className="h-[300px] relative right-28" src={example1} alt="" />
-                <img className=" h-[300px] relative  left-28" src={example2} alt="" />
+            <div className="grid justify-center items-center content-center p-6 md:p-0">
+                <img className="h-[220px] lg:h-[300px] relative right-6 md:right-28" src={example1} alt="" />
+                <img className="h-[220px] lg:h-[300px] relative left-6 md:left-28" src={example2} alt="" />
             </div>
 
         </section>
-        <section className="grid grid-cols-2  ">
+        <section className="grid grid-cols-1 md:grid-cols-2  ">
            
-            <div className="grid justify-center items-center content-center gap-8">
-                <img className="h-[280px] relative left-28" src={example3} alt="" />
-                <img className=" h-[280px] relative right-28 " src={example4} alt="" />
+            <div className="grid justify-center items-center content-center gap-8 p-4 md:p-0 order-2 md:order-1">
+                <img className="h-[180px] lg:h-[280px] relative left-4 md:left-28" src={example3} alt="" />
+                <img className="h-[180px] lg:h-[280px] relative right-4 md:right-28 " src={example4} alt="" />
             </div>
-            <div className=" grid content-center justify-items-center bg-[#DCFCDF]  rounded-l-[8rem] ">
+            <div className=" grid content-center justify-items-center bg-[#DCFCDF]  rounded-l-[8rem] order-1 md:order-2">
                 <div className="w-[70%] m-10 grid gap-8">
                     <h2 className="font-black text-[32px] sm:text-[58px] leading-none text-[#22BC33] ">Comandos personalizados y Avanzados</h2>
                     <p className="font-semibold text-base sm:text-[26px] text-left  ">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: '400px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
     extend: {},
   },
   plugins: [],
