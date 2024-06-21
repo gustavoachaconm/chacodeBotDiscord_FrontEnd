9ee8f0002aab6969406ec5afb4b2baf5f069f46a
GlendyT <glendytuyuc@gmail.com>
2024-06-20 18:44:00 -0600

    iniciar el navbar, instalando react-router-dom para la navegacion, responsive aun falta

---
+++ src/views/LoginButton.jsx
@@ -0,0 +1,17 @@
+import logoDiscrod from "../assets/discrodlogo.png"
+
+export default function LoginButton() {
+    return (
+        <>
+
+            <div className='bg-black text-white rounded-2xl py-1 px-10 max-sm:px-2'>
+              <a className=' flex flex-row gap-6 items-center justify-center font-bold' href="https://discord.com/oauth2/authorize?client_id=1250839124466991136&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fprueba&scope=identify+email+guilds+applications.commands.permissions.update">
+              <h1 className=" max-sm:hidden ">Login</h1>
+              <img src={logoDiscrod} className="w-10"/>
+              </a>
+
+            </div>
+
+        </>
+      )
+    }
