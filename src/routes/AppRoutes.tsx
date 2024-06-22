457684e60bb28f60d2483dfe7f774ef882dfd357
Nicolaxmode <nicolasrojasx.x@gmail.com>
2024-06-21 22:18:17 -0500

    beta front

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import React from 'react';
 import Dashboard from '../pages/Dashboard.tsx'
+import Homepage from '../pages/HomePage.tsx'
 import FooterComponent from "../components/FooterComponent.tsx"
 import Navbar from "../components/Navbar/Navbar.tsx"
 import FooterAuthTrue from '../components/FooterAuthTrue/FooterAuthTrue.tsx'
@@ -13,7 +14,8 @@ const AppRoutes: React.FC = () => {
 
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
       <FooterComponent />
 
