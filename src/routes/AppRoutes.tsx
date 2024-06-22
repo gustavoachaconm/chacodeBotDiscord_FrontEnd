aa764574cc0045db2f4f7163824cf7b2f4cc814e
Nicolaxmode <nicolasrojasx.x@gmail.com>
2024-06-22 16:39:12 -0500

    beta

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Dashboard from '../pages/Dashboard.tsx'
 import Homepage from '../pages/HomePage.tsx'
 import FooterComponent from "../components/FooterComponent.tsx"
-import Navbar from "../components/Navbar/Navbar.tsx"
+import Navbar from "../components/Navbar/Navbar.jsx"
 import FooterAuthTrue from '../components/FooterAuthTrue/FooterAuthTrue.tsx'
 
 
