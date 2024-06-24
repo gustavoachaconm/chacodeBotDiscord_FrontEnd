1f49375937cee60ad9903561255159dbc459bb89
Martin Santiago Aguirre <martinavendano095@gmail.com>
2024-06-24 13:48:52 -0500

    feat:add cardDescription change colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,18 @@
 import heroBot3d from "../assets/heroBot3d.png"
 const Hero = () => {
   return (
-    <div className="bg-[#052E0B]">
+    <div className="">
          <section className="flip-scale-2-hor-top container flex flex-col items-center px-6 py-12 mx-auto   lg:flex-row h-screen">
       <div className="lg:w-1/2 p-6  ">
-        <h1 className="max-w-xl  text-4xl font-medium tracking-wide text-white capitalize md:text-6xl ">Haz que tu <span className="text-[#22BC33] "> Server </span>  de Discord sea mas <span className="text-[#22BC33] "> profesional</span> </h1>
+        <h1 className="max-w-xl  text-4xl font-bold tracking-wide text-white capitalize md:text-6xl ">
+          Haz que tu <span className="text-[#22BC33] "> Server </span>  de Discord sea mas <span className="text-[#22BC33] "> profesional</span> </h1>
    
         <div className="mt-6 sm:flex sm:items-center pt-6">
-          <a href="#" className="bg-[#22BC33] hover:bg-[#24792e]/80 duration-300 transition-colors border-2 border-[#88ec94] px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white rounded">
+          <a href="#" className="bg-[#22BC33] hover:bg-[#24792e]/80 duration-300 transition-colors border-2 border-white px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white rounded">
           Añadir a Discord
           </a>
 
-          <a href="#" className="bg-[#22BC33] hover:bg-[#24792e]/80  border-2 border-[#88ec94] px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white rounded  duration-300 transition-colors  hover:text-white  mt-4 sm:mt-0  sm:mx-4  ">
+          <a href="#" className="bg-[#22BC33] hover:bg-[#24792e]/80  border-2 border-white px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white rounded  duration-300 transition-colors  hover:text-white  mt-4 sm:mt-0  sm:mx-4  ">
           Mas información
           </a>
         </div>
